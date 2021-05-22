c8237de655711e7c2e4eaebca73c11135aba9bba
Reinaldo Usadani <reinaldo.bbu20@gmail.com>
2021-05-23 02:01:14 +0700

    Added some boilerplate to the screens

--- App.js
+++ App.js
@@ -1,21 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import React from "react";
+import AccountScreen from "./src/screens/AccountScreen";
+import SigninScreen from "./src/screens/SigninScreen";
+import SignupScreen from "./src/screens/SignupScreen";
+import TrackCreateScreen from "./src/screens/TrackCreateScreen";
+import TrackDetailsScreen from "./src/screens/TrackDetailsScreen";
+import TrackListScreen from "./src/screens/TrackListScreen";
---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const SignupScreen = () => {
+  return <Text style={{ fontSize: 48 }}>SignupScreen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default SignupScreen;
