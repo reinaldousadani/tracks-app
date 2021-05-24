f09fd68e87236b2f777367e80efca622d15943b0
Reinaldo Usadani <reinaldo.bbu20@gmail.com>
2021-05-24 21:48:42 +0700

    Created some context functions

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,6 +7,31 @@ const authReducer = (state, action) => {
   }
 };
 
+const signup = (dispatch) => {
+  return ({ email, password }) => {
+    // try to signup with the given email and password
+    // update state
+    // show error message if error
+    dispatch();
+  };
+};
+
+const signin = (dispatch) => {
+  return ({ email, password }) => {
+    // try to signin with the given email and password
+    // update state
+    // show error message somehow
+    dispatch();
+  };
+};
+
+const signout = (dispatch) => {
+  return () => {
+    // somehow sign out
+    dispatch();
+  };
+};
+
 export const { Context, Provider } = createDataContext(
   authReducer,
   {},
