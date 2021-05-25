501d8693fa4f5fd5f348486ff248eed7d3446c90
Reinaldo Usadani <reinaldo.bbu20@gmail.com>
2021-05-25 22:34:38 +0700

    implemented async storage to signup action function

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,10 +1,13 @@
 import createDataContext from "./createDataContext";
 import trackerApi from "./../api/tracker";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const authReducer = (state, action) => {
   switch (action.type) {
     case "ADD_ERROR":
       return { ...state, errorMessage: action.payload };
+    case "SIGNUP":
+      return { errorMessage: "", token: action.payload };
     default:
       return state;
   }
@@ -17,7 +20,8 @@ const signup = (dispatch) => {
         email: email,
         password: password,
       });
-      console.log(response.data);
+      await AsyncStorage.setItem("token", response.data.token);
+      dispatch({ type: "SIGNUP", payload: response.data.token });
     } catch (err) {
       dispatch({
         type: "ADD_ERROR",
@@ -46,5 +50,5 @@ const signout = (dispatch) => {
 export const { Context, Provider } = createDataContext(
   authReducer,
   { signup, signin, signout },
-  { isSignedIn: false, errorMessage: "" }
+  { token: null, errorMessage: "" }
 );
