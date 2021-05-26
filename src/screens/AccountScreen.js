4a5a2f1bc39dcef77b02728e397ff27910776dc5
Reinaldo Usadani <reinaldo.bbu20@gmail.com>
2021-05-27 06:48:50 +0700

    Added SafeAreaView/paddingTop to the AccountScreen

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -2,17 +2,21 @@ import React, { useContext } from "react";
 import { Context as AuthContext } from "../context/AuthContext";
 import { Button } from "react-native-elements";
 import Spacer from "../components/Spacer";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Platform } from "react-native";
+import { SafeAreaView } from "react-navigation";
 
 const AccountScreen = () => {
   const { signout } = useContext(AuthContext);
   return (
-    <>
+    <SafeAreaView
+      forceInset={{ top: "always" }}
+      style={{ paddingTop: Platform.OS === "android" ? 25 : 0 }}
+    >
       <Text style={{ fontSize: 48 }}>AccountScreen</Text>
       <Spacer>
         <Button title="SignOut" onPress={signout} />
       </Spacer>
-    </>
+    </SafeAreaView>
   );
 };
 
