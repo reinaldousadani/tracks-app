90b6362243ec4d009579d1cab91d381012fbd6c1
Reinaldo Usadani <reinaldo.bbu20@gmail.com>
2021-06-02 23:06:20 +0700

    Added onPress handler to navigate and pass information to TrackDetailsScreen

--- src/screens/TrackDetailsScreen.js
+++ src/screens/TrackDetailsScreen.js
@@ -1,10 +1,25 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import { StyleSheet } from "react-native";
+import { Context as TrackContext } from "../context/TrackContext";
+import { Text } from "react-native-elements";
 
-const TrackDetailsScreen = () => {
-  return <Text style={{ fontSize: 48 }}>TrackDetailsScreen</Text>;
+const TrackDetailsScreen = ({ navigation }) => {
+  const { state } = useContext(TrackContext);
+  const _id = navigation.getParam("_id");
+  const track = state.find((track) => track._id === _id);
+  return (
+    <Text h2 style={{ textAlign: "center" }}>
+      {track.name}
+    </Text>
+  );
 };
 
 const styles = StyleSheet.create({});
+
+TrackDetailsScreen.navigationOptions = () => {
+  return {
+    title: "Track Details",
+  };
+};
 
 export default TrackDetailsScreen;
