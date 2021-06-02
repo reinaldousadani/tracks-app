8bf35a4ed3501a979c98002469af1967828a4908
Reinaldo Usadani <reinaldo.bbu20@gmail.com>
2021-06-02 22:43:15 +0700

    Fetched tracks data from DB and display it on TrackListScreen

--- src/context/TrackContext.js
+++ src/context/TrackContext.js
@@ -3,13 +3,18 @@ import trackerApi from "../api/tracker";
 
 const trackReducer = (state, action) => {
   switch (action.type) {
+    case "FETCH_TRACKS":
+      return action.payload;
     default:
       return state;
   }
 };
 
 const fetchTracks = (dispatch) => {
-  return () => {};
+  return async () => {
+    const response = await trackerApi.get("/tracks");
+    dispatch({ type: "FETCH_TRACKS", payload: response.data });
+  };
 };
 
 const createTrack = (dispatch) => {
--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,18 +1,46 @@
-import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useContext } from "react";
+import { FlatList, TouchableOpacity } from "react-native";
+import { Context as TrackContext } from "../context/TrackContext";
+import { NavigationEvents } from "react-navigation";
+import { ListItem, Text } from "react-native-elements";
+import { SafeAreaView } from "react-navigation";
 
 const TrackListScreen = ({ navigation }) => {
+  const { state, fetchTracks } = useContext(TrackContext);
+
   return (
-    <>
-      <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
-      <Button
-        title="go to track details"
-        onPress={() => navigation.navigate("TrackDetails")}
+    <SafeAreaView
+      forceInset={{ top: "always" }}
+      style={{ paddingTop: Platform.OS === "android" ? 25 : 0 }}
+    >
+      <NavigationEvents onWillFocus={fetchTracks} />
+      <Text h2 style={{ textAlign: "center" }}>
+        Track Screen
+      </Text>
+      <FlatList
+        data={state}
+        keyExtractor={(item) => item._id}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity>
+              <ListItem>
+                <ListItem.Content>
+                  <ListItem.Title>{item.name}</ListItem.Title>
+                </ListItem.Content>
+                <ListItem.Chevron />
+              </ListItem>
+            </TouchableOpacity>
+          );
+        }}
       />
-    </>
+    </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({});
+TrackListScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
 
 export default TrackListScreen;
