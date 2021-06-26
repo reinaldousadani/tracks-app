7fee38a8a49ce573f7ec9c622fadfb93d4cc39b9
Reinaldo Usadani <reinaldo.bbu20@gmail.com>
2021-06-26 22:41:23 +0700

    Finalize

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const instance = axios.create({
-  baseURL: "http://7c752a813ead.ngrok.io",
+  baseURL: "http://aa15b2f35c28.ngrok.io/",
 });
 
 instance.interceptors.request.use(
